9ee3cff4699bf6dc5d846bbf515098c641a49405
Daniel Okkonen <daniel.okkonen@gmail.com>
2026-08-21 17:23:40 +0200

    Fixes

--- src/backend/types/db.ts
+++ src/backend/types/db.ts
@@ -1,24 +1,24 @@
-export interface DbGame {
-  id: number | null;
-  name: string;
-  franchise: string;
-  start: string | null;
-  end: string | null;
-  created: string | null;
-  updated: string | null;
-  coverImage: string | null;
-  developer: string | null;
-  genres: string | null;
-  platforms: string | null;
-  publisher: string | null;
-  summary: string | null;
-  appId: string | null;
-  playtime_minutes: number;
+export class DbGame {
+  id: number | null = null;
+  name!: string;
+  franchise!: string;
+  start: string | null = null;
+  end: string | null = null;
+  created: string | null = null;
+  updated: string | null = null;
+  coverImage: string | null = null;
+  developer: string | null = null;
+  genres: string | null = null;
+  platforms: string | null = null;
+  publisher: string | null = null;
+  summary: string | null = null;
+  appId: string | null = null;
+  playtime_minutes: number = 0;
 }
 
-export interface DbToken {
-  service: string;
-  token: string;
-  type: string;
-  expires_at: number;
+export class DbToken {
+  service!: string;
+  token!: string;
+  type!: string;
+  expires_at!: number;
 }
--- src/backend/types/igdb.ts
+++ src/backend/types/igdb.ts
@@ -1,28 +1,26 @@
-export interface IgdbCover {
-  url: string;
+export class IgdbCover {
+  url!: string;
 }
 
-export interface IgdbCompany {
-  company: {
-    name: string;
-  };
-  developer: boolean;
-  publisher: boolean;
-  supporting: boolean;
+export class IgdbCompany {
+  company!: { name: string };
+  developer!: boolean;
+  publisher!: boolean;
+  supporting!: boolean;
 }
 
-export interface IgdbGenre {
-  name: string;
+export class IgdbGenre {
+  name!: string;
 }
 
-export interface IgdbPlatform {
-  name: string;
+export class IgdbPlatform {
+  name!: string;
 }
 
-export interface IgdbGame {
-  cover: IgdbCover;
-  summary: string;
-  involved_companies: IgdbCompany[];
-  genres: IgdbGenre[];
-  platforms: IgdbPlatform[];
+export class IgdbGame {
+  cover!: IgdbCover;
+  summary!: string;
+  involved_companies!: IgdbCompany[];
+  genres!: IgdbGenre[];
+  platforms!: IgdbPlatform[];
 }
